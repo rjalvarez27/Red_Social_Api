8981cb09c5668c87afc95f8e1016bba2b04d5203
Raúl Alvarez <raul509@gmail.com>
2024-05-20 22:51:21 -0400

    update avatar

--- app/routes/avatar.js
+++ app/routes/avatar.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const avatarModel = require('../models/avatar');
 const router = express.Router();
-const { getAvatar, avatarPatch, avatarDelete} = require('../controllers/avatar');
+const { getAvatar,  avatarDelete} = require('../controllers/avatar');
 const multer = require('multer');
 
 //localhost:3000/api/avatar
 
 router.get('/:id', getAvatar)
-router.patch('/:id', avatarPatch)
 router.delete('/:id', avatarDelete)
-
 router.post('/', multer().single('avatar'), async (req, res) => {
     try {
         images = req.files.map(file => ({
@@ -19,7 +17,6 @@ router.post('/', multer().single('avatar'), async (req, res) => {
           }));
         const newPublish = await avatarModel .create({
             id_user: req.body.id_user,
-            content: req.body.content,
             avatar: images    
           });
         await newPublish.save();
