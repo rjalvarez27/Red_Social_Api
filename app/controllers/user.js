ed8d0c3f09e5dcd520c6048cc65b48e98490e246
Raúl Alvarez <raul509@gmail.com>
2024-05-27 20:02:58 -0400

    Update user.js

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -49,7 +49,6 @@ const getUser = async (req, res) => {
 const userPatch = async (req, res) => {
   try {
     const { id } = req.params;
-    console.log(id)
     const { name, username, email, rol, password, premium } = req.body;
     const user = await userModel.findByIdAndUpdate(id, {
       name,
@@ -63,7 +62,6 @@ const userPatch = async (req, res) => {
       return res.status(404).json({ message: "Usuario no encontrado" });
     }
     res.status(200).json({ message: "Usuario actualizado exitosamente" });
-    console.log("cambiado con exito")
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
