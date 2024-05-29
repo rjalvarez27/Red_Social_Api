dee48d4c64d36501164eeed0ac7ab5c43e64f43c
Raúl Alvarez <raul509@gmail.com>
2024-05-29 18:11:23 -0400

    creacion de payment y rutas

--- app/controllers/paymentPM.js
+++ app/controllers/paymentPM.js
@@ -1,8 +1,72 @@
 const paymentModel = require('../models/paymentPM');
 
+const createPayment = async (req, res) => {
+    try {
+      const {name, ci, bank, email, type } = req.body;
+      console.log(name, ci, bank, type)
+      const pay = await paymentModel.create({
+        name,
+        ci,
+        bank,
+        email,
+        type
+      });
+      res.status(200).json({ message: "Pago Registrado", pay });
+    } catch (error) {
+      res.status(404).json({ message: "Error al crear un usuario" });
+    }
+  };
+
+
 const getPayment = async (req, res) => {
     const payment = await paymentModel.find();
     res.status(200).json(payment);
 }
 
-module.exports = { getPayment }
+const getPaymentId = async (req, res) => {
+    try {
+      const id = req.params.id;  
+      console.log(id)
+      const user = await paymentModel.findById(id);
+      if (!user) {
+        return res.status(404).json({ message: "Usuario no encontrado" });  
+      }  
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Error al obtener el usuario" });  
+    }  
+  };  
+
+
+
+  const patchPayment = async (req, res) => {
+    try {
+      const id = req.params.id;  
+      const update = req.body;
+      const response = await paymentModel.findByIdAndUpdate(id, update);
+      if (!response) {
+        return res.status(404).json({ message: "Registro no actualizado" });  
+      }  
+      res.status(200).json({ message: "Registro actualizado" });
+    }
+      catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+
+  const deletePayment = async (req, res) => {
+    try {
+      const {id} = req.params;
+      const deleteUser = await paymentModel.findByIdAndDelete(id);
+      if (!deleteUser) {
+        return res.status(404).json({ message: "Registro no encontrado" });
+      }
+      res.json({ message: "registro eliminado exitosamente", deleteUser });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+
+
+
+module.exports = { getPayment, getPaymentId, patchPayment, deletePayment, createPayment }
--- app/routes/paymentPM.js
+++ app/routes/paymentPM.js
@@ -1,41 +1,20 @@
 const express = require('express');
 const router  =  express.Router();
-const multer = require('multer');
-
 const paymentModel = require('../models/paymentPM');
-const { getPayment } = require('../controllers/paymentPM');
+const { getPayment, getPaymentId, patchPayment, deletePayment, createPayment } = require('../controllers/paymentPM');
 
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage });
+
+
 
 router.get('/', getPayment);
 
-router.post('/', upload.array('comprobante', 1), async (req, res) =>{
-   
-    try {
-      images = req.files.map(file => ({
-        name: file.originalname,
-        data: file.buffer,
-        contentType: file.mimetype
-      }));
-  
-      const newPayment = await paymentModel.create({
-        username: req.body.username,
-        email: req.body.email,
-        references: req.body.references,
-        comprobante: images
-        
-      });
-  
-      await newPayment.save();
-      console.log(images)
-      res.send('File enviado' + newPayment);
-    }catch (err) {
-      console.error(err);
-      res.sendStatus(err.status);
-    }
-  
-  });
+router.get('/:id', getPaymentId);
+
+router.patch('/:id', patchPayment);
+
+router.delete('/:id', deletePayment);
+
+router.post('/', createPayment);
 
 module.exports = router
 
