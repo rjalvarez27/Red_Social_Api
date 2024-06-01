b3af274b4c0b3428e545bdd4f3e71c9c17987a09
Raúl Alvarez <raul509@gmail.com>
2024-06-01 03:21:33 -0400

    update bill

--- app/controllers/bill.js
+++ app/controllers/bill.js
@@ -4,19 +4,18 @@ const transporter = require("../../config/mailer");
 const user = require("../models/user");
 
 const postBill = async (req, res) => {
-    const correo = req.params.email;
     const {name, ci, bank, reference, email, type } = req.body;
     try {
         await transporter.sendMail({
             from: '"Mounts" <Mounts@gmail.com>',
-            to: `${correo}`,
+            to: `${email}`,
             subject: `Factura de compra de ${name} ✔`,
             text: `Hola ${name}, los datos ingresados son los siguientes: 
-                   cedula: ${ci} 
-                   mercantil: ${bank} 
-                   numero de referancia: ${reference} 
-                   correo electronico: ${email}
-                   tipo de cuenta: ${type}
+                   Cedula: ${ci} 
+                   Banco: ${bank} 
+                   Numero de referancia: ${reference} 
+                   Correo electronico: ${email}
+                   Tipo de cuenta: ${type}
             Los datos estan siendo procesados por Mounts. Una vez validados los datos, recibiras un email de confirmacion. 
             Gracias por usar Mounts
             `,
